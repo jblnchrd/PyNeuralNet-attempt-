c81df2ae5169db445512c514f31fa668594de6a6
jblnchrd <jblnchrd910@live.com>
2016-12-12 20:17:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	"""
+12 December 2016
+Author: Jacob T. Blanchard
+"""
 
 import numpy as np
 import random as rand
